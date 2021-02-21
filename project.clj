011525b7a4a4e7f3ac2a5fe4335d1d911e6c4244
Yehonathan Sharvit <viebel@gmail.com>
2021-02-21 13:13:17 +0200

    declare the deps as "provided"

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject viebel/duct-google-api "0.1.3"
+(defproject viebel/duct-google-api "0.1.4"
   :description "Integrant methods for google-api-clj"
   :url "https://github.com/viebel/duct-google-api"
   :license {:name "EPL-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[integrant "0.7.0"]
-                 [org.clojure/clojure "1.10.0"]
-                 [viebel/google-api-clj "0.1.3"]])
+            :url  "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[integrant "0.7.0" :scope "provided"]
+                 [org.clojure/clojure "1.10.0" :scope "provided"]
+                 [viebel/google-api-clj "0.1.3" :scope "provided"]])
